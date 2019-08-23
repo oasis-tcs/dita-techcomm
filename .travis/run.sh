6f6b8a9704f54f16b7be773d6abf3865378cc408
Robert D Anderson <robander@us.ibm.com>
2019-08-23 12:40:24 -0500

    First draft of travis test
    Signed-off-by: Robert D Anderson <robander@us.ibm.com>

---
+++ .travis/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+export DITA_HOME=$PWD/dita-ot-$DITA_OT_VERSION
+
+# Build 1.3 doctype to ensure builds working properly
+#$DITA_HOME/bin/dita -i $PWD/metadita-sampledocs-master/doctypes/dita1.3/dtd/base/basemap.ditamap -f html5
+
+# Build beta 2.0 doctype
+$DITA_HOME/bin/dita -i $PWD/metadita-sampledocs-master/doctypes/dita20/2.0grammars-techcomm.ditamap -f html5 --processing-mode=strict
+$DITA_HOME/bin/dita -i $PWD/specification/dita-2.0-technical-content-specification.ditamap -f html5 --processing-mode=strict
